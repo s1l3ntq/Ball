c948ebe31378210da51bffd8dff2fbf3f1b3b2cb
s1l3ntq <qimiller89@gmail.com>
2021-12-18 11:47:14 -0600

    texture added to the sphere.

--- src/components/ball/index.js
+++ src/components/ball/index.js
@@ -1,13 +1,30 @@
 import React from "react";
+import {useLoader} from '@react-three/fiber';
+
+import GlowDot from '../.././assests/textures/glow_dot.jpg';
+import Gradiant from '../.././assests/textures/gradiant.jpg';
+import Plust from '../.././assests/textures/plust.jpg';
+import ToyCar from '../.././assests/textures/toy_car.jpg';
+import { TextureLoader } from "three";
+
+
+
 
 
 export function Ball(props) {
+
+   const [colorBall, glowBall, toyBall, plusBall] = useLoader(
+      TextureLoader, [Gradiant, GlowDot, ToyCar, Plust]
+      );
+
+
    return (
       <>
          <ambientLight intensity ={.5}  />
          <mesh>
           <sphereGeometry args={[1, 32, 32]}/>
-          <meshPhongMaterial color="white" />
+          <meshPhongMaterial colorBall={colorBall} />
+          <meshStandardMaterial map={plusBall} />
          </mesh>
       </>
    );
