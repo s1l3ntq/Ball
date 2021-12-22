078f567b95487cefb84c56c39621b44169a379d8
s1l3ntq <qimiller89@gmail.com>
2021-12-22 12:24:10 -0600

    Rotation of the table component.

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,21 +1,33 @@
 import React from "react";
 import {  OrbitControls } from "@react-three/drei";
-import { TextureLoader } from "three";
+import { TextureLoader, Vector3 } from "three";
 
 
 export function Table (props) {
 
+    
 
     return (
 
 
         <>
 
-        <ambientLight intensity ={.5} />
+         <ambientLight intensity ={.5} />
 
-         <mesh>
-            <boxGeometry args={[1,5,2]} />
+
+         <mesh
+           rotation = {[0, 4.8, 0]} 
+           position = {[0, -3, 0]}>
+            <boxGeometry args={[1, 1, 0]} />
             <meshBasicMaterial color= "green" />
+            <OrbitControls
+            enableZoom={true}
+            enablePan={true}
+            enableRotate={true}
+            zoomSpeed={0.6}
+            panSpeed={0.5}
+            rotateSpeed={0.4}
+             />
          </mesh>
         </>
 
